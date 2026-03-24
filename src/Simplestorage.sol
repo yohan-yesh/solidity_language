d926ca58757b28c086f2270201fbf948e1cc541d
yohan-yesh <yohannesyeshanew04@gmail.com>
2026-03-24 17:23:03 +0300

    easy access of favorite number with help of struct and mapping

--- src/Simplestorage.sol
+++ src/Simplestorage.sol
@@ -3,14 +3,30 @@ pragma solidity 0.8.31;
 
 contract SimpleStorage {
   uint myFavoriteNumber;
+  uint[] listOfFavoriteNumber;
+
+
+    struct Person{
+        uint favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+  mapping(string => uint) public nameToFavoriteNumber;
 
   function store(uint _myFavoriteNumber) public {
-    myFavoriteNumber = _myFavoriteNumber;
+    listOfFavoriteNumber.push(_myFavoriteNumber);
 
   }
 
-  function retrieve() public view returns(uint) {
-    return myFavoriteNumber;
+  function retrieve(uint index) public view returns(uint) {
+    return listOfFavoriteNumber[index];
+
+  }
+
+  function addPerson(string memory _name, uint number) public {
+    listOfPeople.push(Person(number, _name));
+    nameToFavoriteNumber[_name] = number;
 
   }
 
